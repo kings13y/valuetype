2b61280690886b574042fc91455a5fd061a62705
James Shade <james.shade@digital.voa.gov.uk>
2016-08-05 09:21:34 +0100

    Added apply mixin trait for use on options valuetypes.
    Depending on how the option values are implemented you may or may
    not want an apply method on the companion object.
    
    The "of" method is always there by default in the Options trait, and
    performs the same purpose, but without Scala's syntactic sugar that
    is available for apply methods.
    
    If the options are implemented using a case class, then you already
    have an apply method.
    
    However if the options are implemented using case objects inheriting
    a common (normally sealed) trait, then having an apply method is useful.
    
    The Apply trait (and subtraits StringApply and IntApply) allows an apply
    method to be easily mixed into Options companion objects.

--- src/main/scala/uk/gov/voa/valuetype/Options.scala
+++ src/main/scala/uk/gov/voa/valuetype/Options.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.voa.valuetype
 
 import scala.language.implicitConversions
@@ -17,3 +33,18 @@ trait Options[T, VT <: ValueType[T]] {
 trait StringOptions[VT <: StringValue] extends Options[String, VT]
 
 trait IntOptions[VT <: IntValue] extends Options[Int, VT]
+
+trait Apply[T, VT <: ValueType[T]] {
+
+  self: Options[T, VT] =>
+
+  final def apply(x: T): VT = of(x)
+}
+
+trait StringApply[VT <: StringValue] extends Apply[String, VT] {
+  self: StringOptions[VT] =>
+}
+
+trait IntApply[VT <: IntValue] extends Apply[Int, VT] {
+  self: IntOptions[VT] =>
+}
